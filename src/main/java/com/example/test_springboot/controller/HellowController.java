62ae249ac7b6290a4d6e6954e30e1b9e980d45ce
minsu-zip <alstn4814@gmail.com>
2019-08-14 16:27:58 +0900

    l

--- src/main/java/com/example/test_springboot/controller/HellowController.java
+++ src/main/java/com/example/test_springboot/controller/HellowController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HellowController {
     @RequestMapping(value = "/resthellow")
     public String hellow() {
-        return "welcome Hellow World !! ";
+        return "welcome to minsu World !! ";
     }
 }
