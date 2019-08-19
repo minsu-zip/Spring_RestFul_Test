6d28d8de4976dc14ecfe20b76897740305f8abee
minsu-zip <alstn4814@gmail.com>
2019-08-19 13:51:23 +0900

    kk

---
+++ src/main/java/com/example/test_springboot/controller/project.java
@@ -0,0 +1,21 @@
+package com.example.test_springboot.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin
+@RestController
+public class project {
+    @RequestMapping(value = "/remote/project", method = RequestMethod.POST)
+    public Object actionMethod(){
+        RestTemplate restTemplate = new RestTemplate();
+        Object resultObject = new Object();
+        String keyid = "bf33cd622a6f84f143c300c0757e43d3";
+        String targeturi = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid="+keyid;
+        resultObject = restTemplate.getForObject(targeturi, Object.class);
+        return resultObject;
+    }
+}
---
+++ src/main/java/com/example/test_springboot/controller/twoproject2.java
@@ -0,0 +1,21 @@
+package com.example.test_springboot.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin
+@RestController
+public class twoproject2 {
+    @RequestMapping(value = "/remote/twoproject2", method = RequestMethod.GET)
+    public Object actionMethod(){
+        RestTemplate restTemplate = new RestTemplate();
+        Object resultObject = new Object();
+        String keyid = "bf33cd622a6f84f143c300c0757e43d3";
+        String targeturi = "https://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid="+keyid;
+        resultObject = restTemplate.getForObject(targeturi, Object.class);
+        return resultObject;
+    }
+}
